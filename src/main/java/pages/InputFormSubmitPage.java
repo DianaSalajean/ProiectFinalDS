f4f146af3da261bb3a04eb928e49e56e3ff8f05f
DianaSalajean <dya8664@gmail.com>
2023-07-17 04:22:31 -0700

    commit

--- src/main/java/pages/InputFormSubmitPage.java
+++ src/main/java/pages/InputFormSubmitPage.java
@@ -1,36 +1,32 @@
 package pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class InputFormSubmitPage {
+    public InputFormSubmitPage(WebDriver driver) {
+        super(driver);
+    }
     public void fillNameField(String test) {
     }
-
     public void fillEmailField(String mail) {
     }
-
     public void fillPasswordField(String pass) {
     }
-
     public void fillCompanyField(String testCompany) {
     }
-
     public void fillWebsiteField(String s) {
     }
-
     public void fillCityField(String cluj) {
     }
-
     public void fillFirstAddressField(String firstAddress) {
     }
-
     public void fillSecondAddressField(String secondAddress) {
     }
-
     public void fillStateField(String cluj) {
     }
-
     public void fillZipField(String number) {
     }
-
     public void clickSubmitButton() {
     }
 }

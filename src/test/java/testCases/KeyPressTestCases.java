9633d58642e00f2f89c877709b4114a70dfdf20a
DianaSalajean <dya8664@gmail.com>
2023-07-19 03:10:23 -0700

    commit

--- src/test/java/testCases/KeyPressTestCases.java
+++ src/test/java/testCases/KeyPressTestCases.java
@@ -1,15 +1,17 @@
 package testCases;
 
-import pages.BasePage;
-import pages.KeyPressPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.BasePage;
+import pages.KeyPressPage;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+
+import static java.awt.event.KeyEvent.VK_ENTER;
 
 public class KeyPressTestCases extends BasePage {
     private KeyPressPage keyPressPage;
@@ -26,6 +28,8 @@ public class KeyPressTestCases extends BasePage {
         driver.findElement(By.linkText("Key Press")).click();
         keyPressPage.clickOnField();
         WebElement resultElement = driver.findElement(By.xpath("//*[@id=\"my_field\"]"));
+        Robot robot = new Robot();
+        robot.keyPress(VK_ENTER);
         String actualResultText = resultElement.getText();
         String expectedResultText = "You entered: ENTER";
         Assert.assertEquals(actualResultText, expectedResultText, "Entered text is incorrect");
